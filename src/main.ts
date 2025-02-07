e4236f6419bd2d4ba625c58e08e72dcad370444d
MOsama2003 <mo842825@gmail.com>
2025-02-08 03:09:55 +0500

    k

--- src/main.ts
+++ src/main.ts
@@ -2,20 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { JwtAuthGuard } from './auth/guards/jwt.guard';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { ExpressAdapter } from '@nestjs/platform-express';
-import * as express from 'express';
-import { VercelRequest, VercelResponse } from '@vercel/node';
-
-const server = express();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
+  const app = await NestFactory.create(AppModule);
 
   // Enable JWT Auth Guard globally
   app.useGlobalGuards(new JwtAuthGuard());
 
-  app.enableCors();
-
+  app.enableCors()
   // Swagger Configuration
   const config = new DocumentBuilder()
     .setTitle('AgriSense')
@@ -24,7 +18,7 @@ async function bootstrap() {
     .build();
 
   const document = SwaggerModule.createDocument(app, config, {
-    deepScanRoutes: true,
+    deepScanRoutes: true
   });
   SwaggerModule.setup('api/docs', app, document);
 
@@ -32,8 +26,3 @@ async function bootstrap() {
 }
 
 bootstrap();
-
-// ✅ Correct way to export the Vercel handler
-export default (req: VercelRequest, res: VercelResponse) => {
-  server(req, res); // Express handles the request
-};
