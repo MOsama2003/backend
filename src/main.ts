49562f37851ee066be4ccc7ef9928e447472a536
MOsama2003 <mo842825@gmail.com>
2025-02-08 02:17:59 +0500

    made few changes

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,20 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { JwtAuthGuard } from './auth/guards/jwt.guard';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import * as express from 'express';
+import { VercelRequest, VercelResponse } from '@vercel/node';
+
+const server = express();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
+
+  // Enable JWT Auth Guard globally
   app.useGlobalGuards(new JwtAuthGuard());
 
+  app.enableCors()
+  // Swagger Configuration
   const config = new DocumentBuilder()
     .setTitle('AgriSense')
     .setVersion('1.0')
@@ -18,9 +27,12 @@ async function bootstrap() {
   });
   SwaggerModule.setup('api/docs', app, document);
 
-  await app
-    .listen(process.env.PORT ?? 3000)
-    .then(() => console.log('App is running on PORT 3000'))
-    .catch((e) => console.log('Error'));
+  await app.init();
 }
+
 bootstrap();
+
+// Export Vercel handler
+export default async function handler(req: VercelRequest, res: VercelResponse) {
+  server(req, res);
+}
