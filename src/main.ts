824ed16daed8c257bda25081ed416a4d027906d8
MOsama2003 <mo842825@gmail.com>
2025-03-09 03:27:16 +0500

    mde chnge

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { JwtAuthGuard } from './auth/guards/jwt.guard';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ExpressAdapter } from '@nestjs/platform-express';
 import express from 'express';
@@ -9,34 +8,25 @@ async function bootstrap() {
   const server = express();
   const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
 
-  app.useGlobalGuards(new JwtAuthGuard());
   app.enableCors();
 
   const config = new DocumentBuilder()
-    .setTitle('AgriSense')
+    .setTitle('AgriSense API')
     .setVersion('1.0')
     .addBearerAuth()
     .build();
 
-  const document = SwaggerModule.createDocument(app, config, { deepScanRoutes: true });
+  const document = SwaggerModule.createDocument(app, config);
 
   SwaggerModule.setup('api/docs', app, document, {
     customCssUrl: 'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css',
-    customfavIcon: 'https://swagger.io/favicon-32x32.png',
-    customSiteTitle: 'AgriSense API Docs',
     swaggerOptions: {
-      dom_id: '#swagger-ui',
-      url: 'https://agrisense-wheat.vercel.app/api/docs-json', // Replace with your API URL
-      presets: [
-        'SwaggerUIBundle.presets.apis',
-        'SwaggerUIStandalonePreset'
-      ],
-      layout: 'BaseLayout',
       deepLinking: true
     }
   });
 
-  return server; // Important for Vercel
+  await app.init();  // ⚠️ Important for Vercel compatibility
+  return server;
 }
 
 export default bootstrap();
