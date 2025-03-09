1209c32d73e85c5610665d0c5064635bf45aee61
MOsama2003 <mo842825@gmail.com>
2025-03-09 14:48:16 +0500

    revert changes

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { JwtAuthGuard } from './auth/guards/jwt.guard';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import express from 'express';
 import { ConfigService } from '@nestjs/config';
+import path from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const server = express(); // ✅ Use correct syntax
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
 
   const configService = app.get(ConfigService);
   const port = configService.get<number>('PORT') || 3000; // Default to 3000 if not set
@@ -15,6 +19,9 @@ async function bootstrap() {
 
   // Enable CORS
   app.enableCors();
+
+  // Serve Swagger UI Assets Manually
+  app.use('/swagger-ui', express.static(path.join(__dirname, '../public/swagger-ui')));
 
   // Swagger Configuration
   const config = new DocumentBuilder()
@@ -27,7 +34,10 @@ async function bootstrap() {
     deepScanRoutes: true
   });
 
-  SwaggerModule.setup('api/docs', app, document);
+  SwaggerModule.setup('api/docs', app, document, {
+    customJs: '/swagger-ui/swagger-ui-bundle.js',
+    customCssUrl: '/swagger-ui/swagger-ui.min.css',
+  });
 
   await app.listen(port, '0.0.0.0').then(() => 
     console.log(`App is working on http://localhost:${port}/api/docs`)
