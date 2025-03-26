28a06609dd374c48e984d852c5ffa3c2856775ab
Syed Muhammad Ammar <syedmammar123@gmail.com>
2025-03-26 18:00:17 +0500

    basic groq setting done!

--- src/consult-ai-chat/consult-ai-chat.controller.ts
+++ src/consult-ai-chat/consult-ai-chat.controller.ts
@@ -1,14 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { Controller, Get, Query, Request, UseGuards } from '@nestjs/common';
+import { ApiBearerAuth, ApiTags, ApiQuery } from '@nestjs/swagger';
 import { ConsultAiChatService } from './consult-ai-chat.service';
+import { JwtAuthGuard } from 'src/auth/guards/jwt.guard';
 
 @ApiTags('test') 
 @Controller('testChat')
 export class ConsultAiChatController {
   constructor(private readonly consultAiChatService: ConsultAiChatService) {}
 
-  @Get('test')
-  testChat() {
-    return { message: 'Chat API is working!' };
+  @UseGuards(JwtAuthGuard) 
+  @ApiBearerAuth()
+  @Get()
+  @ApiQuery({ name: 'message', type: String, required: true }) // 🛠 Add this for Swagger UI
+  testChat(@Request() req, @Query('message') message: string) {
+    return this.consultAiChatService.getChatResponse(req.user.id, message);
   }
 }
