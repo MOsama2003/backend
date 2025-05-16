37bfb041cfabf95f5cf5fe21267dcc6cf482ca18
MOsama2003 <mo842825@gmail.com>
2025-05-16 07:36:40 +0500

    zxczxc

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -69,7 +69,6 @@ export class AuthController {
 
     user.refreshToken = refreshToken;
     if (fcmToken) {
-      console.log(fcmToken,'osama')
       await this.userRepository.save({ ...user, fcmToken });
       await this.notificationService.subscribeToGlobalNotifications(fcmToken);
     } else {
