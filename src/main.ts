16936086373b77a4034bd7a43f2efd4669dd4f7a
MOsama2003 <mo842825@gmail.com>
2025-05-16 08:07:08 +0500

    asdfasdf

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
 
   const configService = app.get(ConfigService);
-  const port = configService.get<number>('PORT') || 3000; // Default to 3000 if not set
+  const port = process.env.PORT || configService.get<number>('PORT') || 3000;
 
   // Enable JWT Auth Guard globally
   app.useGlobalGuards(new JwtAuthGuard());
@@ -21,7 +21,10 @@ async function bootstrap() {
   app.enableCors();
 
   // Serve Swagger UI Assets Manually
-  app.use('/swagger-ui', express.static(path.join(__dirname, '../public/swagger-ui')));
+  app.use(
+    '/swagger-ui',
+    express.static(path.join(__dirname, '../public/swagger-ui')),
+  );
 
   // Swagger Configuration
   const config = new DocumentBuilder()
@@ -31,17 +34,15 @@ async function bootstrap() {
     .build();
 
   const document = SwaggerModule.createDocument(app, config, {
-    deepScanRoutes: true
+    deepScanRoutes: true,
   });
 
   SwaggerModule.setup('api/docs', app, document, {
     customJs: '/swagger-ui/swagger-ui-bundle.js',
     customCssUrl: '/swagger-ui/swagger-ui.min.css',
   });
-
-  await app.listen(port, '0.0.0.0').then(() => 
-    console.log(`App is working on http://localhost:${port}/api/docs`)
-  );
+  await app.listen(port, '0.0.0.0');
+  console.log(`🚀 App running on port ${port}`);
 }
 
 bootstrap();
